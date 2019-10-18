ee43b501bd78c9aeb74327312149ebf91e1ec442
Daniel <daniel@test.no>
2019-10-19 01:30:31 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
 
-import HomePage from './pages/homepage/homepage.component';
+import HomePage from "./pages/homepage/homepage.component";
 
 function App() {
   return (
     <div>
-      Test
-           <HomePage />
+      <HomePage />
     </div>
   );
 }
