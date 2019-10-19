250ae981d3e50e1fcf2b9fbfb1d29d098f92c897
Daniel <daniel@test.no>
2019-10-20 01:07:51 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Switch, Route } from "react-router-dom";
 import "./App.css";
 
 import HomePage from "./pages/homepage/homepage.component";
@@ -7,8 +8,10 @@ import ShopPage from "./pages/shop/shop.component";
 function App() {
   return (
     <div>
-      <ShopPage />
-      <HomePage />
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route exact path="/shop" component={ShopPage} />
+      </Switch>
     </div>
   );
 }
