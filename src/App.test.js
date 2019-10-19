64277f66be6bd924020148e87d9aad7d8c56ab9d
Daniel <daniel@test.no>
2019-10-19 03:26:16 +0200

    Update App.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 
-it("Kjører Krasj uten å krasje (skal bli rød)", () => {
+it("Kjører Krasj uten å krasje (skal ikke bli rød)", () => {
   const div = document.createElement("div");
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
