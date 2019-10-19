368998e054c9f4c21dc1585b544a734968022440
Daniel <daniel@test.no>
2019-10-20 01:56:09 +0200

    Update shop.component.jsx

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -2,19 +2,20 @@ import React from "react";
 import SHOP_DATA from "./shop.data";
 
 class ShopPage extends React.Component {
-    constructor(props)
-    {
+  constructor(props) {
     super(props);
 
     this.state = {
-        collections: SHOP_DATA
-    }
-
-render()
-{
-    return <div><h1>Butikkside</h1></div>
-}
-
+      collections: SHOP_DATA
+    };
+  }
+  render() {
+    return (
+      <div>
+        <h1>Butikkside</h1>
+      </div>
+    );
+  }
 }
 
 export default ShopPage;
