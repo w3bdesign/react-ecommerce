b7f179dfc7c695dbf6f996486f2aa57ad1f1a271
w3bdesign <daniel@test.no>
2019-11-12 04:53:25 +0100

    Add Preview of shop items

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import SHOP_DATA from "./shop.data";
+import CollectionPreview from "../../components/collection-preview/collection-preview.component";
 
 class ShopPage extends React.Component {
   constructor(props) {
@@ -10,9 +11,12 @@ class ShopPage extends React.Component {
     };
   }
   render() {
+    const { collections } = this.state;
     return (
-      <div>
-        <h1>Butikkside</h1>
+      <div className="shop-page">
+        {collections.map(({ id, ...otherCollectionProps }) => (
+          <CollectionPreview key={id} {...otherCollectionProps} />
+        ))}
       </div>
     );
   }
