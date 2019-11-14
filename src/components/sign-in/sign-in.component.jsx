a743573b9cb0118512debd5c901e72bcaada1917
w3bdesign <daniel@test.no>
2019-11-14 05:27:11 +0100

    Add Firebase and Google auth

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -3,6 +3,7 @@ import React from "react";
 import FormInput from "../form-input/form-input.component";
 import CustomButton from "../custom-button/custom-button.component";
 
+import { signInWithGoogle } from "../../firebase/firebase.utils";
 import "./sign-in.styles.scss";
 
 class SignIn extends React.Component {
@@ -49,6 +50,9 @@ class SignIn extends React.Component {
             required
           />
           <CustomButton type="submit"> Innlogging </CustomButton>
+          <CustomButton onClick={signInWithGoogle}>
+            Google Pålogging
+          </CustomButton>
         </form>
       </div>
     );
