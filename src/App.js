6bcd3eee6cefadbbaceeec389a8fd4462757c5fb
w3bdesign <daniel@test.no>
2019-11-15 22:19:42 +0100

    Finish adding userAuth to Firestore and add the user object to setState

--- src/App.js
+++ src/App.js
@@ -22,9 +22,23 @@ class App extends React.Component {
 
   // TODO Fix memory leak here?
   componentDidMount() {
-    this.unsubscribeFromAuth = auth.onAuthStateChanged(async user => {
-      //this.setState({ currentUser: user });
-      createUserProfileDocument(user);
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
+      if (userAuth) {
+        const userRef = await createUserProfileDocument(userAuth);
+
+        userRef.onSnapshot(snapShot => {
+          this.setState({
+            currentUser: {
+              id: snapShot.id,
+              ...snapShot.data()
+            }
+          });
+        });
+      } else {
+        this.setState({
+          currentUser: userAuth
+        });
+      }
     });
   }
 
