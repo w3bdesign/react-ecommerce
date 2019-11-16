914f9ef745d1a58336747329ed9c5329f8572cd6
w3bdesign <daniel@test.no>
2019-11-16 06:30:19 +0100

    Add support for signing in with email/password through Firebase

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -3,7 +3,7 @@ import React from "react";
 import FormInput from "../form-input/form-input.component";
 import CustomButton from "../custom-button/custom-button.component";
 
-import { signInWithGoogle } from "../../firebase/firebase.utils";
+import { auth, signInWithGoogle } from "../../firebase/firebase.utils";
 import "./sign-in.styles.scss";
 
 class SignIn extends React.Component {
@@ -16,9 +16,17 @@ class SignIn extends React.Component {
     };
   }
   // Update the state when the form is submitted
-  handleSubmit = event => {
+  handleSubmit = async event => {
     event.preventDefault();
-    this.setState({ email: "", password: "" });
+
+    const { email, password } = this.state;
+    try {
+      await auth.signInWithEmailAndPassword(email, password);
+      // Set default state while we wait for signInWithEmailAndPassword() to resolve
+      this.setState({ email: "", password: "" });
+    } catch (error) {
+      console.log(error);
+    }
   };
   // Update the state when the form input changes
   handleChange = event => {
