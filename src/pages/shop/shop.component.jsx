e4699006ba0913b25ed6d45aa462de6020eeaf76
w3bdesign <daniel@test.no>
2019-11-16 04:51:22 +0100

    Adding a README.md to each directory to explain what everything does better

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,13 +1,14 @@
-import React from 'react';
+import React from "react";
 
-import SHOP_DATA from './shop.data.js';
+import SHOP_DATA from "./shop.data.js";
 
-import CollectionPreview from '../../components/collection-preview/collection-preview';
+import CollectionPreview from "../../components/collection-preview/collection-preview";
 
 class ShopPage extends React.Component {
   constructor(props) {
     super(props);
-
+    // Import SHOP_DATA array into state from shop.data.js.
+    // This gives us some products to display
     this.state = {
       collections: SHOP_DATA
     };
@@ -16,7 +17,7 @@ class ShopPage extends React.Component {
   render() {
     const { collections } = this.state;
     return (
-      <div className='shop-page'>
+      <div className="shop-page">
         {collections.map(({ id, ...otherCollectionProps }) => (
           <CollectionPreview key={id} {...otherCollectionProps} />
         ))}
