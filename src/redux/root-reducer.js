8a0e147fd854cc74008a30802f7fcff37a4b4533
w3bdesign <daniel@test.no>
2019-11-17 04:24:48 +0100

    Finish adding Redux to our project. Adding it to index.js as well.

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -1,1 +1,14 @@
-// Our root reducer binds all of our code together
+/*
+The combineReducers helper function turns an object whose values are different reducing functions into a single reducing function you can pass to createStore.
+
+The resulting reducer calls every child reducer, and gathers their results into a single state object.
+
+See https://redux.js.org/api/combinereducers
+*/
+
+import { combineReducers } from "redux";
+import UserReducer from "./user/user.reducer";
+
+export default combineReducers({
+  user: UserReducer
+});
