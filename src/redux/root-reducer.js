0cdac1381de539481f5f4841dd6fa46d978cab77
w3bdesign <daniel@test.no>
2019-11-17 05:23:27 +0100

    Finish Redux reducer setup

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -4,6 +4,10 @@ The combineReducers helper function turns an object whose values are different r
 The resulting reducer calls every child reducer, and gathers their results into a single state object.
 
 See https://redux.js.org/api/combinereducers
+
+Reducers specify how the application's state changes in response to actions sent to the store. 
+Remember that actions only describe what happened, but don't describe how the application's state changes.
+In Redux, all the application state is stored as a single object. It's a good idea to think of its shape before writing any code.
 */
 
 import { combineReducers } from "redux";
--- src/redux/user/user.reducer.js
+++ src/redux/user/user.reducer.js
@@ -1,3 +1,10 @@
+/*
+Reducers specify how the application's state changes in response to actions sent to the store. 
+Remember that actions only describe what happened, but don't describe how the application's state changes.
+
+In Redux, all the application state is stored as a single object. It's a good idea to think of its shape before writing any code.
+*/
+
 // Set a default initial state
 const INITIAL_STATE = {
   currentUser: null
