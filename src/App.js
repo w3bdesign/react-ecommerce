091301ccf72a6a71778296c95183a208359d1236
w3bdesign <daniel@test.no>
2019-11-18 05:58:16 +0100

    Added Redux support for our Header component

--- src/App.js
+++ src/App.js
@@ -3,6 +3,11 @@ import { Switch, Route } from "react-router-dom";
 import { BrowserRouter } from "react-router-dom";
 import { auth, createUserProfileDocument } from "./firebase/firebase.utils";
 
+// Connect Redux to our header
+import { connect } from "react-redux";
+
+import { setCurrentUser } from "../src/redux/user/user.actions";
+
 import "./App.css";
 
 import HomePage from "./pages/homepage/homepage.component";
@@ -11,38 +16,28 @@ import SignInAndSignUpPage from "./pages/sign-in-and-sign-up/sign-in-and-sign-up
 import Header from "./components/header/header.component";
 
 class App extends React.Component {
-  constructor() {
-    super();
-    // Set default state (currentUser) when App is constructed
-    this.state = {
-      currentUser: null
-    };
-  }
-
   unsubscribeFromAuth = null;
 
   // TODO Fix memory leak here?
   componentDidMount() {
     // Add user object to state when Google authentication state has changed.
     // This allows us to be able to use the data later in our application.
+
+    const { setCurrentUser } = this.props;
+
     this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
       if (userAuth) {
         const userRef = await createUserProfileDocument(userAuth);
-        // Add listener to Firestore DocumentSnapShot event and set the state to the currentUser
+        // Add listener to Firestore DocumentSnapShot event and set the Redux store to the currentUser
         userRef.onSnapshot(snapShot => {
-          this.setState({
-            currentUser: {
-              id: snapShot.id,
-              ...snapShot.data()
-            }
+          setCurrentUser({
+            id: snapShot.id,
+            ...snapShot.data()
           });
-          // console.log(this.state);
-        });
-      } else {
-        this.setState({
-          currentUser: userAuth
         });
       }
+
+      setCurrentUser(userAuth);
     });
   }
   // Call unsubscribeFromAuth() to update the state when we destroy the App component
@@ -55,7 +50,7 @@ class App extends React.Component {
     return (
       <div>
         <BrowserRouter>
-          <Header currentUser={this.state.currentUser} />
+          <Header />
           <Switch>
             <Route exact path="/" component={HomePage} />
             <Route path="/shop" component={ShopPage} />
@@ -67,4 +62,8 @@ class App extends React.Component {
   }
 }
 
-export default App;
+const mapDispatchToProps = dispatch => ({
+  setCurrentUser: user => dispatch(setCurrentUser(user))
+});
+
+export default connect(null, mapDispatchToProps)(App);
--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,5 +1,9 @@
 import React from "react";
 import { Link } from "react-router-dom";
+
+// Connect Redux to our header
+import { connect } from "react-redux";
+
 import { auth } from "../../firebase/firebase.utils";
 
 import { ReactComponent as Logo } from "../../assets/crown.svg";
@@ -33,4 +37,22 @@ const Header = ({ currentUser }) => (
   </div>
 );
 
-export default Header;
+// We want to access our currentUser from our Redux reducer:
+const mapStateToProps = state => ({
+  currentUser: state.user.currentUser
+});
+
+/*
+The connect() function connects a React component to a Redux store.
+
+It provides its connected component with the pieces of the data it needs from the store (state), and the functions it can use to dispatch actions to the store.
+
+It does not modify the component class passed to it; instead, it returns a new, connected component class that wraps the component you passed in.
+
+If your mapStateToProps function is declared as taking two parameters, it will be called whenever the store state changes or when the wrapper 
+component receives new props (based on shallow equality comparisons). 
+It will be given the store state as the first parameter, and the wrapper component's props as the second parameter.
+
+https://react-redux.js.org/api/connect
+*/
+export default connect(mapStateToProps)(Header);
