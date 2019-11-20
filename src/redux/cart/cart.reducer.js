690e95f8434a3b5b653f106de4d9be6cfdd53ce5
w3bdesign <daniel@test.no>
2019-11-20 05:30:28 +0100

    Made it so that we are adding cart items to the Redux store

--- src/redux/cart/cart.reducer.js
+++ src/redux/cart/cart.reducer.js
@@ -10,13 +10,20 @@ https://redux.js.org/basics/reducers
 import CartActionTypes from "./cart.types";
 
 const INITIAL_STATE = {
-  hidden: true
+  hidden: true,
+  cartItems: []
 };
 
 const cartReducer = (state = INITIAL_STATE, action) => {
   switch (action.type) {
     case CartActionTypes.TOGGLE_CART_HIDDEN:
       return { ...state, hidden: !state.hidden };
+    case CartActionTypes.ADD_ITEM:
+      // Add the existing cartItems plus the new items (action.payload) to cartItems array
+      return {
+        ...state,
+        cartItems: [...state.cartItems, action.payload]
+      };
     default:
       return state;
   }
