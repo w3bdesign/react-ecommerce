8f1e7d370e29413fb334a2b6a362503444ae1c94
w3bdesign <daniel@test.no>
2019-11-20 05:30:28 +0100

    Made it so that we are adding cart items to the Redux store

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { ReactComponent as ShoppingIcon } from "../../assets/shopping-bag.svg";
 import { connect } from "react-redux";
 
-import toggleCartHidden from "../../redux/cart/cart.actions";
+import { toggleCartHidden } from "../../redux/cart/cart.actions";
 import "./cart-icon.styles.scss";
 
 // We want to access toggleCartHidden from our Redux reducer:
