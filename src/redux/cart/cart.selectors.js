c504cf212df3d4b2ca06e2b9eb0e8c1081bd9296
w3bdesign <daniel@test.no>
2019-11-23 05:59:35 +0100

    Npm upgrade, added and installed Reselect and setup our cart to use it

---
+++ src/redux/cart/cart.selectors.js
@@ -0,0 +1,26 @@
+/*
+Simple “selector” library for Redux (and others) inspired by getters in NuclearJS, subscriptions in re-frame and this proposal from speedskater.
+
+Selectors can compute derived data, allowing Redux to store the minimal possible state.
+Selectors are efficient. A selector is not recomputed unless one of its arguments changes.
+Selectors are composable. They can be used as input to other selectors.
+*/
+
+import { createSelector } from "reselect";
+
+const selectCart = state => state.cart;
+
+export const selectCartItems = createSelector(
+  [selectCart],
+  cart => cart.cartItems
+);
+
+export const selectCartItemsCount = createSelector(
+  [selectCartItems],
+  cartItems =>
+    cartItems.reduce(
+      (accumulatedQuantity, cartItem) =>
+        accumulatedQuantity + cartItem.quantity,
+      0
+    )
+);
