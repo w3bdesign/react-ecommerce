0d30c8cc7eb58994ce3cf290007aba7f14752148
w3bdesign <daniel@test.no>
2019-11-23 06:32:39 +0100

    Updated several components with mapStateToProps to use createStructuredSelector

--- src/redux/user/user.selectors.js
+++ src/redux/user/user.selectors.js
@@ -8,24 +8,9 @@ Selectors are composable. They can be used as input to other selectors.
 
 import { createSelector } from "reselect";
 
-const selectCart = state => state.cart;
+const selectUser = state => state.user;
 
-export const selectCartItems = createSelector(
-  [selectCart],
-  cart => cart.cartItems
-);
-
-export const selectCartItemsCount = createSelector(
-  [selectCartItems],
-  cartItems =>
-    cartItems.reduce(
-      (accumulatedQuantity, cartItem) =>
-        accumulatedQuantity + cartItem.quantity,
-      0
-    )
-);
-
-export const selectCartHidden = createSelector(
-  [selectCart],
-  cart => cart.hidden
+export const selectCurrentUser = createSelector(
+  [selectUser],
+  user => user.currentUser
 );
