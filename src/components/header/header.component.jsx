d6a0da6d77ab1cc70c43507ea07e0c40f7ddd3f9
w3bdesign <daniel@test.no>
2019-11-23 06:32:39 +0100

    Updated several components with mapStateToProps to use createStructuredSelector

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -3,11 +3,14 @@ import { Link } from "react-router-dom";
 
 // Connect Redux to our header
 import { connect } from "react-redux";
+import { createStructuredSelector } from "reselect";
 
 import { auth } from "../../firebase/firebase.utils";
 import CartIcon from "../cart-icon/cart-icon.component";
 
 import CartDropDown from "../cart-dropdown/cart-dropdown.component";
+import { selectCartHidden } from "../../redux/cart/cart.selectors";
+import { selectCurrentUser } from "../../redux/user/user.selectors";
 
 import { ReactComponent as Logo } from "../../assets/crown.svg";
 
@@ -42,10 +45,18 @@ const Header = ({ currentUser, hidden }) => (
 );
 
 // We want to access our currentUser from our Redux reducer. A more advanced method of destructuring (nested values).
-const mapStateToProps = ({ user: { currentUser }, cart: { hidden } }) => ({
-  currentUser,
-  hidden
-});
+
+/*
+createStructuredSelector takes an object whose properties are input-selectors and returns a structured selector. 
+The structured selector returns an object with the same keys as the inputSelectors argument, but with the selectors replaced with their values.
+In other words, this function is handy when we are using multiple selectors in the same mapStateToProps function.
+*/
+
+const mapStateToProps = state =>
+  createStructuredSelector({
+    currentUser: selectCurrentUser,
+    hidden: selectCartHidden
+  });
 
 /*
 The connect() function connects a React component to a Redux store.
