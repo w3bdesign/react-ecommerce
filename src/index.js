d5326fc33d6ff40db368ba4cbd6a33b2de649e67
w3bdesign <hijack_xxx@yahoo.com>
2019-12-01 07:51:01 +0100

    Installed Redux-persist, configured it and started to use it in our application

--- src/index.js
+++ src/index.js
@@ -2,16 +2,20 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 
+import { PersistGate } from "redux-persist/integration/react";
+
 import "./index.css";
 import App from "./App";
 
 // Import the store in order to use Redux
-import store from "./redux/store";
+import { store, persistor } from "./redux/store";
 
 // Wrap Redux Provider around our App
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <PersistGate persistor={persistor}>
+      <App />
+    </PersistGate>
   </Provider>,
   document.getElementById("root")
 );
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,7 @@
-/*
-In Redux, all the application state is stored as a single object. There should only be a single store in your app.
-*/
+// In Redux, all the application state is stored as a single object. There should only be a single store in your app.
 
 import { createStore, applyMiddleware } from "redux";
+import { persistStore } from "redux-persist";
 
 // Import the logger which will log actions between the reducer and the components
 import logger from "redux-logger";
@@ -11,13 +10,8 @@ import rootReducer from "./root-reducer";
 
 const middlewares = [logger];
 
-/* 
-Creates a Redux store that holds the complete state tree of your app.
-Apply the logger middleware with applyMiddleware(...middlewares) with [logger] passed
-as an array to be able to easily add additional middleware later.
+export const store = createStore(rootReducer, applyMiddleware(...middlewares));
 
-See https://redux.js.org/api/createstore 
-*/
-const store = createStore(rootReducer, applyMiddleware(...middlewares));
+export const persistor = persistStore(store);
 
-export default store;
+export default { store, persistor };
