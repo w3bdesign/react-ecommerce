7561ef9c153876c78e8c9c79166376a898556f6d
w3bdesign <hijack_xxx@yahoo.com>
2019-12-01 04:00:16 +0100

    General cleanup of code to move comments to README.md

--- src/redux/cart/cart.reducer.js
+++ src/redux/cart/cart.reducer.js
@@ -1,12 +1,3 @@
-/*
-Reducers specify how the application's state changes in response to actions sent to the store. 
-Remember that actions only describe what happened, but don't describe how the application's state changes.
-
-In Redux, all the application state is stored as a single object. It's a good idea to think of its shape before writing any code.
-
-https://redux.js.org/basics/reducers
-*/
-
 import CartActionTypes from "./cart.types";
 import { addItemToCart } from "./cart.utils";
 
--- src/redux/cart/cart.selectors.js
+++ src/redux/cart/cart.selectors.js
@@ -1,11 +1,3 @@
-/*
-Simple “selector” library for Redux (and others) inspired by getters in NuclearJS, subscriptions in re-frame and this proposal from speedskater.
-
-Selectors can compute derived data, allowing Redux to store the minimal possible state.
-Selectors are efficient. A selector is not recomputed unless one of its arguments changes.
-Selectors are composable. They can be used as input to other selectors.
-*/
-
 import { createSelector } from "reselect";
 
 const selectCart = state => state.cart;
