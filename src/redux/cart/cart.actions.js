9dc8163b3d66a15537bebadd47ce63aaa7e7ee2a
w3bdesign <hijack_xxx@yahoo.com>
2019-12-01 04:00:16 +0100

    General cleanup of code to move comments to README.md

--- src/redux/cart/cart.actions.js
+++ src/redux/cart/cart.actions.js
@@ -1,23 +1,3 @@
-/*
-Actions are payloads of information that send data from your application to your store (state). 
-They are the only source of information for the store (state). 
-You send them to the store (state) using store.dispatch().
-
-Action creators are exactly that, functions that create actions. 
-It's easy to conflate the terms “action” and “action creator”, so do your best to use the proper term.
-In Redux, action creators simply return an action:
-
-function addTodo(text) {
-  return {
-    type: ADD_TODO,
-    text
-  }
-}
-
-https://redux.js.org/basics/actions#actions
-
-*/
-
 import CartActionTypes from "./cart.types";
 
 // Note that we are not using payload here as we dont need it
