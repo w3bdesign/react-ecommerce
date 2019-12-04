357f77aa98fc3945955ebdf1a7ce6c06ebe33fae
w3bdesign <hijack_xxx@yahoo.com>
2019-12-04 06:53:17 +0100

    Fix type that prevented categories from working correctly, make it so that selectCollections uses an object

--- src/redux/shop/shop.selectors.js
+++ src/redux/shop/shop.selectors.js
@@ -7,11 +7,15 @@ export const selectCollections = createSelector(
   shop => shop.collections
 );
 
+export const selectCollectionsForPreview = createSelector(
+  [selectCollections],
+  collections => Object.keys(collections).map(key => collections[key])
+);
+
 // Map our URL params based on COLLECTION_ID_MAP
 // /shop/hats = 1, /shop/sneakers = 2 etc
-export const selectCollection = collectionUrlParam => {
+export const selectCollection = collectionUrlParam =>
   createSelector(
     [selectCollections],
     collections => collections[collectionUrlParam]
   );
-};
