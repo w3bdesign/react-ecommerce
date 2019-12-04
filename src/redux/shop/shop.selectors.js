30c1ed0a5f28381241969032becee6d4bebfe4f9
w3bdesign <hijack_xxx@yahoo.com>
2019-12-04 06:31:02 +0100

    Update shop.selectors.js to use object instead of array for shop.data

--- src/redux/shop/shop.selectors.js
+++ src/redux/shop/shop.selectors.js
@@ -1,12 +1,5 @@
 import { createSelector } from "reselect";
 
-const COLLECTION_ID_MAP = {
-  hats: 1,
-  sneakers: 2,
-  jackets: 3,
-  womens: 4,
-  mens: 5
-};
 const selectShop = state => state.shop;
 
 export const selectCollections = createSelector(
@@ -17,9 +10,8 @@ export const selectCollections = createSelector(
 // Map our URL params based on COLLECTION_ID_MAP
 // /shop/hats = 1, /shop/sneakers = 2 etc
 export const selectCollection = collectionUrlParam => {
-  createSelector([selectCollections], collections =>
-    collections.find(
-      collection => collection.id === COLLECTION_ID_MAP[collectionUrlParam]
-    )
+  createSelector(
+    [selectCollections],
+    collections => collections[collectionUrlParam]
   );
 };
