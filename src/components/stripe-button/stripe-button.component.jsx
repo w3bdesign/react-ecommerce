57a00e307b84f93faa38f7e611fa97ff66c80177
w3bdesign <hijack_xxx@yahoo.com>
2019-12-05 04:29:32 +0100

    Finished implementing Stripe

--- src/components/stripe-button/stripe-button.component.jsx
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -7,7 +7,26 @@ const StripeCheckoutButton = ({ price }) => {
   // We fetch the Stripe API key from our .env file
   const publishableKey = process.env.REACT_APP_STRIPEKEY;
 
+  const onToken = token => {
+    console.log(token);
+    alert("Betaling mottatt");
+  };
   return (
-      
-  )
+    <StripeCheckout
+      label="Kjøp nå"
+      name="React Nettbutikk"
+      billingAddress
+      shippingAddress
+      image="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSEo5gcsQGp6j_bE0btl5Jg3ZckLPfzbRPqypp5ry3TWXifCkQ1&s"
+      description={`Total: ${price} kr`}
+      amount={priceForStripe}
+      panelLabel="Kjøp nå"
+      token={onToken}
+      stripeKey={publishableKey}
+      locale="auto"
+      currency="NOK"
+    />
+  );
 };
+
+export default StripeCheckoutButton;
