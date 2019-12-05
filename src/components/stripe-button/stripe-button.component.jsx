9c0396bd142dbfd4c46902445629705ae93d9636
w3bdesign <hijack_xxx@yahoo.com>
2019-12-05 04:03:04 +0100

    Update Readme.md to include Stripe

--- src/components/stripe-button/stripe-button.component.jsx
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -1,2 +1,13 @@
 import React from "react";
 import StripeCheckout from "react-stripe-checkout";
+
+const StripeCheckoutButton = ({ price }) => {
+  const priceForStripe = price * 100;
+
+  // We fetch the Stripe API key from our .env file
+  const publishableKey = process.env.REACT_APP_STRIPEKEY;
+
+  return (
+      
+  )
+};
