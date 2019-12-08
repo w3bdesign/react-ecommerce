6e2c05c3ddefc7c13fd851c1f257250ee10529c0
w3bdesign <hijack_xxx@yahoo.com>
2019-12-08 04:02:59 +0100

    Convert collection item to use styled components

--- src/components/collection-item/collection-item.component.jsx
+++ src/components/collection-item/collection-item.component.jsx
@@ -1,35 +1,37 @@
 import React from "react";
-import CustomButton from "../custom-button/custom-button.component";
-import "./collection-item.styles.scss";
-
-// Redux
 import { connect } from "react-redux";
+
 import { addItem } from "../../redux/cart/cart.actions";
+
+import {
+  CollectionItemContainer,
+  CollectionFooterContainer,
+  AddButton,
+  BackgroundImage,
+  NameContainer,
+  PriceContainer
+} from "./collection-item.styles";
 
 const CollectionItem = ({ item, addItem }) => {
   const { name, price, imageUrl } = item;
+
   return (
-    <div className="collection-item">
-      <div
-        className="image"
-        style={{
-          backgroundImage: `url(${imageUrl})`
-        }}
-      />
-      <div className="collection-footer">
-        <span className="name">{name}</span>
-        <span className="price">kr {price}0</span>
-      </div>
-      <CustomButton onClick={() => addItem(item)} inverted>
-        KJØP
-      </CustomButton>
-    </div>
+    <CollectionItemContainer>
+      <BackgroundImage className="image" imageUrl={imageUrl} />
+      <CollectionFooterContainer>
+        <NameContainer>{name}</NameContainer>
+        <PriceContainer>{price} kr</PriceContainer>
+      </CollectionFooterContainer>
+      <AddButton onClick={() => addItem(item)} inverted>
+        Kjøp
+      </AddButton>
+    </CollectionItemContainer>
   );
 };
 
 // https://react-redux.js.org/using-react-redux/connect-mapdispatch
-
 // Dispatch the new AddItem from Cart Actions
+
 const mapDispatchToProps = dispatch => ({
   addItem: item => dispatch(addItem(item))
 });
